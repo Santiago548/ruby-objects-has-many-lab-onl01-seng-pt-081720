bfd5a931d7f23808a3de1e2e86e0f285de563f95
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-28 15:58:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,22 @@
 
 class Artist
-  attr_accessor :name
+  attr_accessor :name :songs
 
   def initialize(name)
     @name = name
+    @songs = []]
   end
 
   def add_song
-
+    @songs << songs
   end
 
   def add_song_by_name
-
+    @songs.new(name)
+    add_song
   end
 
   def self.song_count
-
+    self.all.song.count
   end
 end
